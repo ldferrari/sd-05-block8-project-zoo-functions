fc3850ccff26ad8da7fa3d1a512f8916950dcfaf
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-06-19 18:34:34 -0300

    funcao createEmployee criada

--- src/zoo.js
+++ src/zoo.js
@@ -50,8 +50,12 @@ function employeeByName(employeeName) {
   return saida;
 }
 
+//Cria um novo colaborador a partir de objetos contendo informações pessoais, gerentes e animais gerenciados
+
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
+  //concatenaEntradas:
+  return Object.assign(personalInfo, associatedWith);
 }
 
 function isManager(id) {
