b292d2958f6102e10e2993cd13e58666bc079ddd
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-06-19 18:36:36 -0300

    climate issue

--- src/zoo.js
+++ src/zoo.js
@@ -50,11 +50,12 @@ function employeeByName(employeeName) {
   return saida;
 }
 
-//Cria um novo colaborador a partir de objetos contendo informações pessoais, gerentes e animais gerenciados
+//  Cria um novo colaborador a partir de objetos contendo
+//  informações pessoais, gerentes e animais gerenciados
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
-  //concatenaEntradas:
+  //  seu código aqui
+  //  concatenaEntradas:
   return Object.assign(personalInfo, associatedWith);
 }
 
