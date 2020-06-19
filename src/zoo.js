02085cb9a3daaf4ab9ccf4636cdcc2fd2d59a1b1
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-06-19 15:19:46 -0300

    Fatorando a função createEmployee.

--- src/zoo.js
+++ src/zoo.js
@@ -34,6 +34,7 @@ function employeeByName(employeeName) {
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
+  return {...personalInfo, ...associatedWith};
 }
 
 function isManager(id) {
