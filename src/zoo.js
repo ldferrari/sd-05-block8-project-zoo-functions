8bf7a07c55de6b7d61c790670212400f6246e481
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-06-19 16:13:47 -0300

    climate issue 5

--- src/zoo.js
+++ src/zoo.js
@@ -27,10 +27,10 @@ function animalsByIds(...ids) {
   }));
   return arraySaida;
 }
-//console.log(animalsByIds());
-//console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
-//console.log('com dois elementos');
-//console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'baa6e93a-f295-44e7-8f70-2bcdc6f6948d'));
+/*  console.log(animalsByIds());
+console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
+console.log('com dois elementos');
+console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'baa6e93a-f295-44e7-8f70-2bcdc6f6948d'));  */
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
