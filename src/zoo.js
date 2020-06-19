75bd99aeb70cf8a3a28c2b1f74d6c9fcdf2bc5f5
pedrohcalado <pedrocalado22@gmail.com>
2020-06-19 18:36:54 -0300

    addEmploye

--- src/zoo.js
+++ src/zoo.js
@@ -53,8 +53,14 @@ function isManager(id) {
   return employees.some(({ managers }) => managers.includes(id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  employees.push({
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  });
 }
 
 function animalCount(species) {
