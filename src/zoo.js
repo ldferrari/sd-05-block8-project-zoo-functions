abb4bd2cf9a3bb2b7fe3ce8b2fa7a18867d8b6ca
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-06-19 16:45:01 -0300

    Correcao codeclimate - identacao

--- src/zoo.js
+++ src/zoo.js
@@ -9,16 +9,14 @@ eslint no-unused-vars: [
 ]
 */
 
-const data = require("./data");
+const data = require('./data');
 
 const { animals, employees } = data;
 
 function animalsByIds(...ids) {
-  const testArray = [];
-  ids.forEach((id) =>
-    testArray.push(...animals.filter((list) => id === list.id))
-  );
-  return testArray;
+	const testArray = [];
+	ids.forEach(id => testArray.push(...animals.filter(list => id === list.id)));
+	return testArray;
 }
 
 /*Ao passar o nome de uma espécie e uma idade, 
@@ -26,61 +24,61 @@ testa se todos os animais desta espécie possuem a idade mínima especificada*/
 function animalsOlderThan(animal, age) {}
 
 function employeeByName(employeeName) {
-  // seu código aqui
+	// seu código aqui
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+	// seu código aqui
 }
 
 function isManager(id) {
-  // seu código aqui
+	// seu código aqui
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+	// seu código aqui
 }
 
 function animalCount(species) {
-  // seu código aqui
+	// seu código aqui
 }
 
 function entryCalculator(entrants) {
-  // seu código aqui
+	// seu código aqui
 }
 
 function animalMap(options) {
-  // seu código aqui
+	// seu código aqui
 }
 
 function schedule(dayName) {
-  // seu código aqui
+	// seu código aqui
 }
 
 function oldestFromFirstSpecies(id) {
-  // seu código aqui
+	// seu código aqui
 }
 
 function increasePrices(percentage) {
-  // seu código aqui
+	// seu código aqui
 }
 
 function employeeCoverage(idOrName) {
-  // seu código aqui
+	// seu código aqui
 }
 
 module.exports = {
-  entryCalculator,
-  schedule,
-  animalCount,
-  animalMap,
-  animalsByIds,
-  employeeByName,
-  employeeCoverage,
-  addEmployee,
-  isManager,
-  animalsOlderThan,
-  oldestFromFirstSpecies,
-  increasePrices,
-  createEmployee,
+	entryCalculator,
+	schedule,
+	animalCount,
+	animalMap,
+	animalsByIds,
+	employeeByName,
+	employeeCoverage,
+	addEmployee,
+	isManager,
+	animalsOlderThan,
+	oldestFromFirstSpecies,
+	increasePrices,
+	createEmployee,
 };
