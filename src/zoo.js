1ab0e9fb01edc4cb11bf59ddc2c46c643bd32ad3
Mau <mauricio.sdjr@gmail.com>
2020-06-19 18:23:06 -0300

    implement createEmployee function

--- src/zoo.js
+++ src/zoo.js
@@ -28,8 +28,9 @@ function employeeByName(employeeName) {
   employeeData.firstName === employeeName || employeeData.lastName === employeeName);
 }
 
+//Cria um novo colaborador a partir de objetos contendo informações pessoais, gerentes e animais gerenciados
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+ return {...personalInfo, ...associatedWith};
 }
 
 function isManager(id) {
