9d9607e11f9954ee8e416a532b83abc02f001289
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-06-19 19:22:13 -0300

    Correcao codeclimate da funcao isManager

--- src/zoo.js
+++ src/zoo.js
@@ -39,7 +39,8 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  const verficaGerente = employees.some(gerente => gerente.managers.find(idGerente => idGerente === id))
+  const verficaGerente = employees.some(gerente => 
+  gerente.managers.find(idGerente => idGerente === id));
   return verficaGerente;
 }
 
