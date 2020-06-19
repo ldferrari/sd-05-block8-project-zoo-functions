2480b09dd57a93881532db97107e7f9e41a18019
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-06-19 15:48:05 -0300

    resolucao da funcao animalsByIds

--- src/zoo.js
+++ src/zoo.js
@@ -11,8 +11,21 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+/*
+- Implemente a função animalsByIds:
+Caso receba nenhum parâmetro, necessário retornar um array vazio
+Ao receber como parâmetro um único id, retorna os animais com este id
+Ao receber mais de um id, retorna os animais que têm um desses ids */
+
+function animalsByIds(...ids) {
+  if (!ids.length) return [];
+  const arraySaida = [];
+  ids.forEach( (cadaId) => data.animals.find( (cadaAnimal) => {
+    if (cadaAnimal.id === cadaId) {
+      arraySaida.push(cadaAnimal);
+    };
+}));
+  return arraySaida;
 }
 
 function animalsOlderThan(animal, age) {
