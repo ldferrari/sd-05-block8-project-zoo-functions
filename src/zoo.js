ac918d6d2f6d46ef47244a8b5d5f5a6cbda5f0de
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-06-19 17:05:39 -0300

    funcao mais limpa

--- src/zoo.js
+++ src/zoo.js
@@ -21,18 +21,10 @@ Ao receber mais de um id, retorna os animais que têm um desses ids */
 function animalsByIds(...ids) {
   if (!ids.length) return [];
   const arraySaida = [];
-  ids.forEach(cadaId => {
-    return animals.find((cadaAnimal) => {
-    if (cadaAnimal.id === cadaId) {
-      arraySaida.push(cadaAnimal);
-    }
-  })});
+  ids.forEach(cadaId => arraySaida.push(animals.find(cadaAnimal => cadaAnimal.id === cadaId)));
   return arraySaida;
 }
-console.log(animalsByIds());
-console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
-console.log('com dois elementos');
-console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'baa6e93a-f295-44e7-8f70-2bcdc6f6948d'));
+
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
