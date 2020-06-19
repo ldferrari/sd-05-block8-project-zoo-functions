7abfff5834b0e448eb84df46b8e4671776af444d
Lucas Costa Ventura <lucascv333@gmail.com>
2020-06-19 15:33:42 -0300

    add ex2

--- src/zoo.js
+++ src/zoo.js
@@ -13,10 +13,14 @@ const data = require('./data');
 
 function animalsByIds(ids) {
   // seu código aqui
+  if (ids === undefined) return [];
+  
+
 }
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
+  return data.animals.filter(lista => lista.name === animal).every(lista => lista.residents.age >= age);
 }
 
 function employeeByName(employeeName) {
