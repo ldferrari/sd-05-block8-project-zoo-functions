7c0fc4ad920c48a042996ba31f46a0e113c86112
Mau <mauricio.sdjr@gmail.com>
2020-06-19 18:06:57 -0300

    codeclimate

--- src/zoo.js
+++ src/zoo.js
@@ -24,7 +24,7 @@ function animalsOlderThan(animal, age) {
 function employeeByName(employeeName) {
   if (!employeeName) return {};
   return employees.find(
-  employeeData => 
+  employeeData =>
   employeeData.firstName === employeeName || employeeData.lastName === employeeName);
 }
 
