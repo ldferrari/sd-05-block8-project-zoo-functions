79cf58ebcf6a01103e49d3876ae84aff660ef18b
César Alves <alvescesar@hotmail.com>
2020-06-19 23:04:06 -0300

    Added functions 'employeeByName' && 'createEmployee'

--- src/zoo.js
+++ src/zoo.js
@@ -11,25 +11,22 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-const { animals } = data;
+const { animals, employees } = data;
 
 function animalsByIds(...ids) {
-  // Caso receba nenhum parâmetro, necessário retornar um array vazio
-  // Ao receber como parâmetro um único id, retorna os animais com este id
-  // Ao receber mais de um id, retorna os animais que têm um desses ids
   return ids.map(id => animals.find(animal => animal.id === id));
 }
 
 function animalsOlderThan(animal, age) {
-  // seu código aqui
 }
 
 function employeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) return {};
+  return employees.find(employee => (employee.firstName === employeeName || employee.lastName === employeeName));
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  return { ...personalInfo, ...associatedWith };
 }
 
 function isManager(id) {
