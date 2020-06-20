6b82263ceb2c0f933b114e2126181467f9155d8b
luiz-avelar <luiz.avelar25@gmail.com>
2020-06-20 16:56:56 -0300

    Second commit

--- src/zoo.js
+++ src/zoo.js
@@ -13,104 +13,93 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-const { animals, employees, prices } = data;
+const { animals, employees, hours, prices } = data;
 
 function animalsByIds(...ids) {
-  // seu código aqui
-
-  // Caso receba nenhum parâmetro, necessário retornar um array vazio
-  // Ao receber como parâmetro um único id, retorna os animais com este id
-  // Ao receber mais de um id, retorna os animais que têm um desses ids
-
-  return ids.map( (id) => (animals.find( (animal) => animal.id === id)) );
-
+  return ids.map(id => animals.find(animal => animal.id === id));
 }
 
 function animalsOlderThan(animal, age) {
-  // seu código aqui
-
-  // Ao passar o nome de uma espécie e uma idade, testa se todos os animais desta espécie possuem a idade mínima especificada
-  
-  const selectedAnimal = animals.find( (element) => element.name === animal);
+  const selectedAnimal = animals.find(element => element.name === animal);
   const { residents } = selectedAnimal;
-  return residents.every( (resident) => resident.age > age);
+  return residents.every(resident => resident.age > age);
 }
 
 function employeeByName(employeeName) {
-  // seu código aqui
-
-  // Sem parâmetros, retorna um objeto vazio
-  // Quando provido o primeiro nome do funcionário, retorna o objeto do funcionário
-  // Quando provido o último nome do funcionário, retorna o objeto do funcionário
-  
-  return employees.find( (employee) => (employee.firstName === employeeName || employee.lastName === employeeName)) || {};
+  if (!employeeName) return {};
+  return employees.find(employee =>
+  employee.firstName === employeeName || employee.lastName === employeeName);
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
-
-  // Cria um novo colaborador a partir de objetos contendo informações pessoais, gerentes e animais gerenciados
-  // personalInfo = id, firstName, lastName
-  // associatedWith = managers, responsibleFor
-  // Expected = {id: '', firstName: '', lastName: '', managers: [], associatedWith: []}
-
   return { ...personalInfo, ...associatedWith };
 }
 
 function isManager(id) {
-  // seu código aqui
-  //Testa se o id passado é de um gerente
-
-  return employees.some ( (employee) => employee.managers.includes(id) );
+  return employees.some(employee => employee.managers.includes(id));
 }
 
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
-  // seu código aqui
-  // Adiciona um funcionário no fim da lista
-
-  return employees.push({id, firstName, lastName, managers, responsibleFor});
-
+  return employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
 function animalCount(species) {
-  // seu código aqui
-  // Sem parâmetros, returna animais e suas quantidades
-  // Com o nome de uma espécie de animal, retorna somente a quantidade
   if (!species) {
     const allAnimals = {};
-    animals.forEach( (animal) => {return allAnimals[animal.name] = animal.residents.length});
+    animals.forEach((animal) => {
+      allAnimals[animal.name] = animal.residents.length;
+    });
     return allAnimals;
-  } else {
-    return (animals.find( (animal) => animal.name === species)).residents.length;
   }
+  return animals.find(animal => animal.name === species).residents.length;
 }
 
 function entryCalculator(entrants) {
-  // seu código aqui
-  // Returna 0 se nenhum argumento for passado
-  // Retorna 0 se um objeto vazio for passado
-  // Retorna o preço total a ser cobrado dado o número de adultos, crianças e idosos
   if (!entrants || Object.entries(entrants).length === 0) return 0;
 
   const pricesArray = Object.entries(prices);
 
-  return pricesArray.reduce( (sum, ageGroup) => sum + ageGroup[1] * entrants[ageGroup[0]], 0);
+  return pricesArray.reduce(
+    (sum, ageGroup) => sum + (ageGroup[1] * entrants[ageGroup[0]]),
+    0);
 }
 
-function animalMap(options) {
-  // seu código aqui
-}
+function animalMap(options) {}
 
 function schedule(dayName) {
-  // seu código aqui
+  const cronograma = Object.entries(hours).reduce((obj, day) => {
+    if (day[1].open === day[1].close) {
+      obj[day[0]] = 'CLOSED';
+    } else {
+      obj[day[0]] = `Open from ${day[1].open}am until ${
+        day[1].close - 12
+      }pm`;
+    }
+    return obj;
+  }, {});
+
+  if (!dayName) return cronograma;
+
+  const singleDay = {};
+  singleDay[dayName] = schedule[dayName];
+
+  return singleDay;
 }
 
 function oldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employeeByID = employees.find(employee => employee.id === id);
+  const responsibleFor = employeeByID.responsibleFor[0];
+  const species = animals.find(animal => animal.id === responsibleFor);
+  const residents = species.residents;
+  residents.sort((a, b) => (a.age > b.age ? -1 : 1));
+  return Object.values(residents[0]);
 }
 
 function increasePrices(percentage) {
-  // seu código aqui
+  return Object.keys(prices).forEach((key) => {
+    prices[key] =
+      Math.round(((prices[key] * (percentage + 100)) / 100) * 100) / 100;
+  });
 }
 
 function employeeCoverage(idOrName) {
