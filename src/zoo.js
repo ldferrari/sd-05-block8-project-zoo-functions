c7793ecd1bca319324787e55f55e94a6333fad24
marinabrcls <barcelos.marina@gmail.com>
2020-06-20 21:17:02 -0300

    Requirement 4 was the easiest so far

--- src/zoo.js
+++ src/zoo.js
@@ -42,6 +42,7 @@ function employeeByName(employeeName) {
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
+  return {...personalInfo, ...associatedWith};
 }
 
 function isManager(id) {
