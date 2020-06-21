86d3dea3cd77ce667a96266c0c5b5c82b08aa53f
RenataNovais <rnovaiscs@gmail.com>
2020-06-21 14:16:29 -0300

    Function addEmployee

--- src/zoo.js
+++ src/zoo.js
@@ -46,8 +46,9 @@ function isManager(id) {
   return employees.some(employee => employee.managers.find(item => item === id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor =[]) {
   // seu código aqui
+  return employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
 function animalCount(species) {
