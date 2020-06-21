116b568230ce6ddff21a27a0719b9447f51fd792
Lucas Costa Ventura <lucascv333@gmail.com>
2020-06-20 23:58:07 -0300

    add isManager

--- src/zoo.js
+++ src/zoo.js
@@ -47,7 +47,10 @@ function createEmployee(personalInfo, associatedWith) {
 
 function isManager(id) {
   // seu código aqui
+  return Object.values(data.employees).some(({ managers }) => managers[0] === id);
 }
+
+console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
