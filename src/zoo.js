ac3f81d7b74a2cec9775947679f68fa5afe9a331
fabiano Alves <fasnaweb2020@gmail.com>
2020-06-22 16:40:35 -0300

    Implemente a função animalsByIds: ok!

--- src/zoo.js
+++ src/zoo.js
@@ -9,22 +9,20 @@ eslint no-unused-vars: [
 ]
 */
 
-const data = require("./data");
+const data = require('./data');
 
-const { animals, employees } = require("./data");
+const { animals } = require('./data');
 
 function animalsByIds(...ids) {
-  let result = ids;
+  const result = ids;
   if (result.length === 1) {
-    return animals.filter((elemento) => elemento.id === result[0]);
+    return animals.filter(elemento => elemento.id === result[0]);
   } else if (result.length >= 2) {
-    return animals.filter(
-      (elemento) => elemento.id === result[0] || elemento.id === result[1]
-    );
+    return animals.filter(elemento => elemento.id === result[0] || elemento.id === result[1]);
   }
   return result;
 }
-
+/*
 function animalsOlderThan(animal, age) {
   // seu código aqui
 }
@@ -71,7 +69,7 @@ function increasePrices(percentage) {
 
 function employeeCoverage(idOrName) {
   // seu código aqui
-}
+}*/
 module.exports = {
   /* entryCalculator,
   schedule,
