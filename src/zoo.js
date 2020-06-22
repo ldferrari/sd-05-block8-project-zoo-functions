2fffa5e24bcfebd4378a788f09d260c80d1e9f0e
matambrosio <matmagamb1@gmail.com>
2020-06-22 16:27:18 -0300

    req 6 push 1

--- src/zoo.js
+++ src/zoo.js
@@ -55,7 +55,9 @@ function isManager(id) {
   // seu código aqui
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  return employees.push{id,firstName,lastName,managers,responsibleFor};
+
   // seu código aqui
 }
 
