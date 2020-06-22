392a663e16a0c89ee51dd6386c06c25cd84d66e4
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-06-22 12:29:01 -0300

    funçao schedule funcionando

--- src/zoo.js
+++ src/zoo.js
@@ -148,21 +148,24 @@ function animalMap(options) {
 //console.log(animalMap({ includeNames: true, sorted: true}));
 
 function schedule(dayName) {
+  const daysWeek = ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday'];
+  let scheduleInform = {'Tuesday': '', 'Wednesday': '', 'Thursday': '', 'Friday': '', 'Saturday': '', 'Sunday': '', 'Monday': ''};
+  daysWeek.forEach((day) => {
+    if (data.hours[day].open !== 0 && data.hours[day].close !== 0) {
+      scheduleInform[day] = `Open from ${data.hours[day].open}am until ${data.hours[day].close - 12}pm`;
+    } else {
+      scheduleInform[day] = 'CLOSED';
+    } 
+  });
   if (dayName === undefined) {
-    const daysWeek = ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday'];
-    let scheduleInform = {'Tuesday': '', 'Wednesday': '', 'Thursday': '', 'Friday': '', 'Saturday': '', 'Sunday': '', 'Monday': ''};
-    daysWeek.forEach((day) => {
-      if (data.hours[day].open !== 0 && data.hours[day].close !== 0) {
-        scheduleInform[day] = `Open from ${data.hours[day].open}am until ${data.hours[day].close - 12}pm`;
-      } else {
-        scheduleInform[day] = 'CLOSED';
-      }
-      
-    });
     return scheduleInform;
+  } else {
+    const oneDaySchedule = {[dayName]: scheduleInform[dayName]};
+    return oneDaySchedule;
   }
+  
 }
-console.log(schedule());
+console.log(schedule('Thursday'));
 
 function oldestFromFirstSpecies(id) {
   // seu código aqui
