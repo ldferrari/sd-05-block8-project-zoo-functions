4046eebc345073086660f0cbf097cf2acb06d839
Christian Duessel <duessel@gmail.com>
2020-06-22 19:31:34 -0300

    increaseP codeclimat2e

--- src/zoo.js
+++ src/zoo.js
@@ -116,9 +116,9 @@ function oldestFromFirstSpecies(id) {
 
 function increasePrices(percentage) {
   // seu código aqui
-  prices.Adult = Math.round( prices.Adult * (1 + (percentage/100)) * 100) / 100;
-  prices.Senior = Math.round(prices.Senior*(1 + (percentage/100)) * 100) / 100;
-  prices.Child = Math.round(prices.Child*(1 + (percentage/100)) * 100) / 100;
+  prices.Adult = Math.round(prices.Adult * (1 + (percentage/100)) * 100) / 100;
+  prices.Senior = Math.round(prices.Senior * (1 + (percentage/100)) * 100) / 100;
+  prices.Child = Math.round(prices.Child * (1 + (percentage/100)) * 100) / 100;
 
   return prices;
 }
