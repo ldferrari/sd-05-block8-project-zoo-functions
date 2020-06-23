501a3fa8b59102251b743952eaf6989a59412e06
Lucas Costa Ventura <lucascv333@gmail.com>
2020-06-22 21:31:23 -0300

    add inscreasePrices

--- src/zoo.js
+++ src/zoo.js
@@ -10,6 +10,8 @@ eslint no-unused-vars: [
 */
 
 const data = require('./data');
+const { employees } = require('./data');
+const { prices } = require('./data');
 
 function animalsByIds(...ids) {
   // seu código aqui
@@ -85,23 +87,50 @@ function animalMap(options) {
   }
 }
 
-console.log(animalMap());
+// console.log(animalMap());
 
 function schedule(dayName) {
   // seu código aqui
+  if (dayName === undefined) {
+    // return Object.keys(data.hours).forEach(hour => console.log(hour));
+  }
 }
+
+// console.log(schedule());
 
 function oldestFromFirstSpecies(id) {
   // seu código aqui
+  let animalMaisVelho = 0;
+  data.employees.find(ids => {
+    if (ids === id) {
+      const especieAnimal = ids.responsibleFor[0];
+      return especieAnimal;
+    }
+  });
 }
+
+// console.log(oldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 function increasePrices(percentage) {
   // seu código aqui
+  Object.keys(prices).forEach(key => {
+    prices[key] = Math.round((prices[key] * 100) * (1 + (percentage / 100))).toFixed(2) / 100;
+  });
+  return prices;
 }
+
+// console.log(increasePrices(50));
 
 function employeeCoverage(idOrName) {
   // seu código aqui
+  if (idOrName === undefined) {
+    const resultado = {};
+    data.employees.forEach(employee => (resultado[`${employee.firstName} ${employee.lastName}`] = []));
+    return resultado;
+  }
 }
+
+// console.log(employeeCoverage());
 
 module.exports = {
   entryCalculator,
