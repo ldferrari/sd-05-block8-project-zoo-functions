3c3ac4ee2a7af545918c3483b31febd85a485d23
Elisa <elisatcamillo@gmail.com>
2020-06-24 00:01:16 -0300

    oldestFromFirstSpecies

--- src/zoo.js
+++ src/zoo.js
@@ -79,42 +79,55 @@ function animalCount(species) {
 }
 
 function entryCalculator(entrants) {
-  if (typeof entrants === 'undefined') { return 0 }
-  else if (typeof entrants === 'object'){
-    if (Object.entries(entrants).length === 0){return 0};
-  }  
+  if (typeof entrants === 'undefined') { return 0; } else
+  if (typeof entrants === 'object') {
+    if (Object.entries(entrants).length === 0) { return 0; }
+  }
 
-  const adults = entrants['Adult'];
-  const children = entrants['Child'];
-  const seniors = entrants['Senior'];
-  const total = (adults * prices['Adult'] + children * prices['Child'] + seniors * prices['Senior']);
+  const adults = entrants.Adult;
+  const children = entrants.Child;
+  const seniors = entrants.Senior;
+  const total = ((adults * prices.Adult) + (children * prices.Child) + (seniors * prices.Senior));
   return total;
 }
 
 function animalMap(options) {
-  
+
 }
 
 function schedule(dayName) {
   const scheduleObj = {
-  'Tuesday': 'Open from 8am until 6pm',
-  'Wednesday': 'Open from 8am until 6pm',
-  'Thursday': 'Open from 10am until 8pm',
-  'Friday': 'Open from 10am until 8pm',
-  'Saturday': 'Open from 8am until 10pm',
-  'Sunday': 'Open from 8am until 8pm',
-  'Monday': 'CLOSED'
-  }
-  if (dayName === undefined) {return scheduleObj};
+    Tuesday: 'Open from 8am until 6pm',
+    Wednesday: 'Open from 8am until 6pm',
+    Thursday: 'Open from 10am until 8pm',
+    Friday: 'Open from 10am until 8pm',
+    Saturday: 'Open from 8am until 10pm',
+    Sunday: 'Open from 8am until 8pm',
+    Monday: 'CLOSED',
+  };
+  if (dayName === undefined) { return scheduleObj; }
   const outputSchedule = {};
   outputSchedule[dayName] = scheduleObj[dayName];
   return outputSchedule;
 }
 
-console.log(schedule())
-
 function oldestFromFirstSpecies(id) {
-  // seu código aqui
+  // findId is each employee, checks if id matches parameter
+  const findEmployee = employees.find(findId => findId.id === id);
+  // the selected employee object has a responsibleFor array of strings
+  // with the ids of the species they're (obvs) responsible for
+  const animalTypeId = findEmployee.responsibleFor[0];
+  // searches in the data.animals array for the animal species
+  // corresponding to the responsibleFor[0] id
+  const findAnimalObj = animals.find(animal => animal.id === animalTypeId);
+  // the species found is an array; the residents property
+  // is another array with the individual animals
+  const animalsArray = findAnimalObj.residents;
+  // sort residents by age
+  const oldestAnimalSort = animalsArray.sort(function (a, b) { return b.age - a.age; });
+  // to return an array with only the obj values instead of the entire object
+  const oldestAnimal = Object.values(oldestAnimalSort[0]);
+  return oldestAnimal;
 }
 
 function increasePrices(percentage) {
