9bbed7003d6c0fa9b076c57472b537e1da651a67
Mau <mauricio.sdjr@gmail.com>
2020-06-24 18:29:43 -0300

    codeclimate

--- src/zoo.js
+++ src/zoo.js
@@ -69,10 +69,11 @@ function schedule(dayName) {
   const completeSchedule = {};
   Object.keys(hours).forEach((day) => {
     if (hours[day].open === hours[day].close) {
-    completeSchedule[day] = 'CLOSED';
-  } else {
-    completeSchedule[day] = `Open from ${ hours[day].open }am until ${ hours[day].close - 12 }pm`}});
-  return dayName ? { [ dayName ]:completeSchedule[dayName] } : completeSchedule;
+      completeSchedule[day] = 'CLOSED';
+    } else {
+     completeSchedule[day] = `Open from ${hours[day].open}am until ${hours[day].close - 12}pm`
+    }});
+  return dayName ? { [dayName]:completeSchedule[dayName] } : completeSchedule;
 }
 
 function oldestFromFirstSpecies(id) {
