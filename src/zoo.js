6a9a6bc52e8a34cc376f1589a477097d4ee66eac
Mau <mauricio.sdjr@gmail.com>
2020-06-24 18:34:30 -0300

    codeclimate

--- src/zoo.js
+++ src/zoo.js
@@ -71,9 +71,10 @@ function schedule(dayName) {
     if (hours[day].open === hours[day].close) {
       completeSchedule[day] = 'CLOSED';
     } else {
-     completeSchedule[day] = `Open from ${hours[day].open}am until ${hours[day].close - 12}pm`
-    }});
-  return dayName ? { [dayName]:completeSchedule[dayName] } : completeSchedule;
+      completeSchedule[day] = `Open from ${hours[day].open}am until ${hours[day].close - 12}pm`;
+    }
+  });
+  return dayName ? { [dayName]: completeSchedule[dayName] } : completeSchedule;
 }
 
 function oldestFromFirstSpecies(id) {
