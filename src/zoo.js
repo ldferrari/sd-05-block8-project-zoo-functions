b0a2ff5ffbef00b64033aeb0e9779e69bc9dba1d
luialbeto <araujo.script@gmail.com>
2020-06-25 13:23:54 -0300

    implementação olderThan

--- src/zoo.js
+++ src/zoo.js
@@ -26,14 +26,19 @@ function animalsByIds(...ids) {
     });
   });
   return armazenaDados;
+  console.log(armazenaDados);
 }
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
+return data.animals.find(index => index.name === animal).residents.every
+(retornoResidents => retornoResidents.age > age);
 }
 
 function employeeByName(employeeName) {
   // seu código aqui
+
+return data.employees.some(armazenaDados => armazenaDados)
 }
 
 function createEmployee(personalInfo, associatedWith) {
