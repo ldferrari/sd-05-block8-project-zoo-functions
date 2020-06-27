40f2f434608b964653435f7915e50a1976f4412d
hleoc <informig@gmail.com>
2020-06-27 14:36:49 -0300

    Ininciando o projeto

--- src/zoo.js
+++ src/zoo.js
@@ -10,10 +10,31 @@ eslint no-unused-vars: [
 */
 
 const data = require('./data');
+const { animals } = require('./data');
 
-function animalsByIds(ids) {
+function animalsByIds(...ids) {
   // seu código aqui
+  if (ids.length === 0) { //ids.length === 0
+    return [];
+  }
+
+  // para um parametro dentro do ids
+  /* for (let i = 0; i < ids.length; i++) {
+    let idProcurado = ids[i];
+    console.log(animals.find(animal => {
+    return animal.id === ids[i];
+    }   
+  ))
+  } */
+  //[1,2,3]
+  let hugo= ids.map(variavelTemp => {
+   return animals.find(animal => animal.id === variavelTemp)
+  })
+  return hugo;
+ 
+  
 }
+animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce')
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
