34513358432ab958baa27f1db5e0a13b2970639a
hleoc <informig@gmail.com>
2020-06-27 15:20:35 -0300

    Ininciando o projeto - exercicio 1

--- src/zoo.js
+++ src/zoo.js
@@ -13,28 +13,14 @@ const data = require('./data');
 const { animals } = require('./data');
 
 function animalsByIds(...ids) {
-  // seu código aqui
-  if (ids.length === 0) { //ids.length === 0
+  if (ids.length === 0) {
     return [];
   }
 
-  // para um parametro dentro do ids
-  /* for (let i = 0; i < ids.length; i++) {
-    let idProcurado = ids[i];
-    console.log(animals.find(animal => {
-    return animal.id === ids[i];
-    }   
-  ))
-  } */
-  //[1,2,3]
-  let hugo= ids.map(variavelTemp => {
-   return animals.find(animal => animal.id === variavelTemp)
-  })
-  return hugo;
- 
-  
+  const animais = ids.map(variavelTemp =>
+    animals.find(animal => animal.id === variavelTemp));
+  return animais;
 }
-animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce')
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
