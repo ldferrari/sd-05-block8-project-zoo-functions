6fe5cb27064513f522eff6a11804ca6096a8554d
Erlingsjunior <erlingsjunior@gmail.com>
2020-06-27 16:46:07 -0300

    climate3

--- src/zoo.js
+++ src/zoo.js
@@ -23,9 +23,8 @@ function animalsByIds(...ids) {
   return animals;
 }
 
-
 function animalsOlderThan(animal, age) {
-  // seu código aqui
+  return animals.find(valor => valor.name === animal).residents.every(valor2 => valor2.age >= age);
 }
 
 function employeeByName(employeeName) {
@@ -48,79 +47,9 @@ function animalCount(species) {
   // seu código aqui
 }
 
-function entryCalculatoconst data = require('./data');
-const { animals } = require('./data');
-
-function animalsByIds(...ids) {
-  if (ids.length === 0) {
-    return [];
-  }
-  return animals
-}
-
-function animalsOlderThan(animal, age) {
+function entryCalculator (){
   // seu código aqui
 }
-
-function employeeByName(employeeName) {
-  // seu código aqui
-}
-
-function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
-}
-
-function isManager(id) {
-  // seu código aqui
-}
-
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
-}
-
-function animalCount(species) {
-  // seu código aqui
-}
-
-function entryCalculator(entrants) {
-  // seu código aqui
-}
-
-function animalMap(options) {
-  // seu código aqui
-}
-
-function schedule(dayName) {
-  // seu código aqui
-}
-
-function oldestFromFirstSpecies(id) {
-  // seu código aqui
-}
-
-function increasePrices(percentage) {
-  // seu código aqui
-}
-
-function employeeCoverage(idOrName) {
-  // seu código aqui
-}
-
-module.exports = {
-  entryCalculator,
-  schedule,
-  animalCount,
-  animalMap,
-  animalsByIds,
-  employeeByName,
-  employeeCoverage,
-  addEmployee,
-  isManager,
-  animalsOlderThan,
-  oldestFromFirstSpecies,
-  increasePrices,
-  createEmployee,
-};
 
 function animalMap(options) {
   // seu código aqui
