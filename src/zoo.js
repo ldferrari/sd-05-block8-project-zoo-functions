961a23cdd1a29e4041de4504176e31536ea1c50c
Cass007 <thecastieel07@gmail.com>
2020-06-29 01:31:43 -0300

    ...

--- src/zoo.js
+++ src/zoo.js
@@ -9,14 +9,14 @@ eslint no-unused-vars: [
 ]
 */
 
-const data = require('./data');
+const data = require("./data");
 
 function animalsByIds(ids) {
   // seu código aqui
   if (ids.length === 0) {
     return [];
   }
-  return animais.filter((an) => ids.map((a) => an.id === id));
+  return animals.filter((an) => ids.find((_) => an.id === id));
 }
 
 function animalsOlderThan(animal, age) {
