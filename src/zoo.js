a5c0c80528ab3fad6d4a0b361bf447ae58383a7d
luca <luucadecastro@gmail.com>
2020-06-28 23:19:11 -0300

    employeeByName

--- src/zoo.js
+++ src/zoo.js
@@ -38,7 +38,7 @@ function employeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  return { ...personalInfo, ...associatedWith};
 }
 
 function isManager(id) {
