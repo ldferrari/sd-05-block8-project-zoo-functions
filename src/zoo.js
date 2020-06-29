204d652931965595c6d61b2b58ff46d43a9fd814
luialbeto <araujo.script@gmail.com>
2020-06-29 19:02:17 -0300

    schedule

--- src/zoo.js
+++ src/zoo.js
@@ -11,7 +11,12 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-const { prices, animals, employees } = data;
+const {
+  prices,
+  animals,
+  employees,
+  hours
+} = data;
 
 function animalsByIds(...ids) {
   // seu código aqui
@@ -41,9 +46,9 @@ function employeeByName(employeeName) {
   if (employeeName === undefined) {
     return {};
   }
-  const employeeEncontrado = employees.find( buscaFuncionario => buscaFuncionario.firstName === employeeName );
-  const employeeEncontrado2 = employees.find( buscaFuncionario => buscaFuncionario.lastName === employeeName );
-  if(employeeEncontrado !== undefined) {
+  const employeeEncontrado = employees.find(buscaFuncionario => buscaFuncionario.firstName === employeeName);
+  const employeeEncontrado2 = employees.find(buscaFuncionario => buscaFuncionario.lastName === employeeName);
+  if (employeeEncontrado !== undefined) {
     return employeeEncontrado;
   } else if (employeeEncontrado2 !== undefined) {
     return employeeEncontrado2;
@@ -66,17 +71,40 @@ function isManager(id) {
 
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   // seu código aqui
-const outroFuncionario = {id, firstName, lastName, managers, responsibleFor}
-const outroFuncionario2 = Object.assign({}, {id}, {firstName}, {lastName}, {managers}, {responsibleFor})
-const outroFuncionario3 = createEmployee({id, firstName, lastName}, {managers, responsibleFor})
-employees.push(outroFuncionario2);
+  const outroFuncionario = {
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor
+  }
+  const outroFuncionario2 = Object.assign({}, {
+    id
+  }, {
+    firstName
+  }, {
+    lastName
+  }, {
+    managers
+  }, {
+    responsibleFor
+  })
+  const outroFuncionario3 = createEmployee({
+    id,
+    firstName,
+    lastName
+  }, {
+    managers,
+    responsibleFor
+  })
+  employees.push(outroFuncionario2);
 }
 
 function animalCount(species) {
   // seu código aqui
   const retornaObjeto = {};
   animals.forEach(animal => (retornaObjeto[animal.name] = animal.residents.length));
-  if(species === undefined) {
+  if (species === undefined) {
     return retornaObjeto;
   }
   return retornaObjeto[species];
@@ -85,14 +113,16 @@ animalCount('lions')
 
 function entryCalculator(entrants) {
   // seu código aqui
-  if(entrants === undefined || Object.keys(entrants).length === 0) {
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
     return 0;
   }
-  const {Adult = 0, Child = 0, Senior = 0} = entrants;
-let total = Adult * prices.Adult
-total += Child * prices.Child
-total += Senior * prices.Senior
-return total;
+  const {
+    Adult = 0, Child = 0, Senior = 0
+  } = entrants;
+  let total = Adult * prices.Adult
+  total += Child * prices.Child
+  total += Senior * prices.Senior
+  return total;
 }
 
 function animalMap(options) {
@@ -101,40 +131,54 @@ function animalMap(options) {
 
 function schedule(dayName) {
   // seu código aqui
-  if (dayName) return { [dayName]: weekDays[dayName] };
-  return weekDays;
+  const crono = Object.assign({}, hours);
+  const dias = Object.keys(crono);
+  dias.forEach(dia => {
+    crono[dia] = `Open from ${crono[dia].open}am until ${crono[dia].close - 12}pm`
+  if (dia === 'Monday') crono[dia] = 'CLOSED'
+})
+  if (dayName === undefined) {
+    return crono;
+  }
+  const saida = {};
+    saida[dayName] = crono[dayName];
+    return saida;
 }
 
-function oldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+  function oldestFromFirstSpecies(id) {
+    // seu código aqui
+  }
 
-function increasePrices(percentage) {
-  // seu código aqui
-  const divisor = percentage / 100;
-  const { Adult, Senior, Child } = prices;
-  prices.Adult = Math.round(Adult * (divisor + 1) * 100) / 100;
-  prices.Senior = Math.round(Senior * (divisor + 1) * 100) / 100;
-  prices.Child = Math.round(Child * (divisor + 1) * 100) / 100;
-  return prices;
-}
+  function increasePrices(percentage) {
+    // seu código aqui
+    const divisor = percentage / 100;
+    const {
+      Adult,
+      Senior,
+      Child
+    } = prices;
+    prices.Adult = Math.round(Adult * (divisor + 1) * 100) / 100;
+    prices.Senior = Math.round(Senior * (divisor + 1) * 100) / 100;
+    prices.Child = Math.round(Child * (divisor + 1) * 100) / 100;
+    return prices;
+  }
 
-function employeeCoverage(idOrName) {
-  // seu código aqui
-}
+  function employeeCoverage(idOrName) {
+    // seu código aqui
+  }
 
-module.exports = {
-  entryCalculator,
-  schedule,
-  animalCount,
-  animalMap,
-  animalsByIds,
-  employeeByName,
-  employeeCoverage,
-  addEmployee,
-  isManager,
-  animalsOlderThan,
-  oldestFromFirstSpecies,
-  increasePrices,
-  createEmployee,
-};
+  module.exports = {
+    entryCalculator,
+    schedule,
+    animalCount,
+    animalMap,
+    animalsByIds,
+    employeeByName,
+    employeeCoverage,
+    addEmployee,
+    isManager,
+    animalsOlderThan,
+    oldestFromFirstSpecies,
+    increasePrices,
+    createEmployee,
+  };
