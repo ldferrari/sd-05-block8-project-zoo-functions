caa1d7e821ceca189a2b5f13f7dbb1f6c850911f
Carla Nakajuni <carlanakajuni@hotmail.com>
2020-06-30 10:39:56 -0300

    Fixing codeclimate

--- src/zoo.js
+++ src/zoo.js
@@ -124,7 +124,7 @@ function oldestFromFirstSpecies(id) {
   const { name, sex, age } = animals.find(animal =>
     animal.id === idAnimal).residents.sort((a, b) =>
     b.age - a.age)[0];
- return [name, sex, age]
+  return [name, sex, age];
 }
 
 function increasePrices(percentage) {
