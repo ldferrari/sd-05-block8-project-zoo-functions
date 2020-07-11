b8607b5f026ee27eda320cb3a8fb90ed5361059e
SamuelPPlab <samuelpp.s@hotmail.com>
2020-07-11 08:17:02 -0300

    code climate

--- src/zoo.js
+++ src/zoo.js
@@ -88,7 +88,7 @@ function oldestFromFirstSpecies(id) {
 }
 
 function increasePrices(percentage) {
-  prices.Adult = Math.round(prices.Adult * (1 + (percentage / 100)) * 100) / 100
+  prices.Adult = Math.round(prices.Adult * (1 + (percentage / 100)) * 100) / 100;
   prices.Child = Math.round(prices.Child * (1 + (percentage / 100)) * 100) / 100;
   prices.Senior = Math.round(prices.Senior * (1 + (percentage / 100)) * 100) / 100;
 }
