6032bf93b1a83f78e8f113f1f925b9ee69bb1de1
Alexander Mitchell <alexlm.trybe@gmail.com>
2020-08-10 09:08:51 -0300

    add semicolon

--- src/zoo.js
+++ src/zoo.js
@@ -61,7 +61,7 @@ function isManager(id) {
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
-  employees.push({ id, firstName, lastName, managers, responsibleFor })
+  employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
 function animalCount(species) {
