a0f06400ba89f90b56e8657f530866af739cb0c2
Alexander Mitchell <alexlm.trybe@gmail.com>
2020-08-11 09:18:10 -0300

    cc...2...

--- src/zoo.js
+++ src/zoo.js
@@ -108,7 +108,7 @@ function schedule(dayName) {
 function oldestFromFirstSpecies(id) {
   // seu código aqui
   const animalID = employees.find(employee => employee.id === id).responsibleFor[0];
-  const {name,sex,age} = aniamls.find(animal => animal.id === animalID).residents.sort((y,o)=> o.age-y.age)[0];
+  const { name, sex, age } = animals.find(animal => animal.id === animalID).residents.sort(( y, o )=> o.age - y.age)[0];
   return [name, sex, age];
 }
 
